ba3999c806fe1f85027c04131260ee5ea87f809f
kath3rine <not.katherine.li@gmail.com>
2023-05-29 00:57:21 -0400

    animation, contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,19 @@
-import '../App.scss';
+import '../styles/Home.css';
 import Test from '../images/test.png';
-import Email from '../images/email.png';
-import Linkedin from '../images/linkedin.png';
-import Github from '../images/github.png';
+
 
 function Home() {
   return (
     <div className="Home">
         <div className="home-section" id="home-text">
             <p>Hi, I'm </p>
-            <h1> Katherine Li</h1>
+            <h1> Katherine</h1>
             <h2> Computer Science + Immersive Media Design student @ University of Maryland </h2>
 
-            <a className="home-button" href="#experience">Experience</a>
-            <a className="home-button" href="#projects">Projects</a>
-
-            <div className="contact">
-            <a href="mailto:kli17@terpmail.umd.edu"><img src={Email}/></a>
-            <a href="https:/linkedin.com/in/kli17"><img src={Linkedin}/></a>
-            <a href="https://github.com/kath3rine"><img src={Github}/></a>
+            <div className="buttons">
+                <a href="#experience">Experience</a>
+                <a href="#projects">Projects</a>
             </div>
-
         </div>
 
         <div className="home-section" id="home-img">
