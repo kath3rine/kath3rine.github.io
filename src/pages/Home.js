df45111f0d73179e570ceb3ec09969a7656a0e6f
kath3rine <not.katherine.li@gmail.com>
2023-06-24 23:37:19 -0400

    pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,29 @@
 import '../styles/Home.css';
 import Katherine from '../images/katherine.png';
 import Test from '../images/maze.jpg';
-import Contact from '../components/Contact.js'
+import Email from '../images/email.png';
+import Linkedin from '../images/linkedin.png';
+import Github from '../images/github.png';
 
 function Home() {
   return (
     <div className="Home">
         <div className="home-section" id="home-text">
             <p>Hi, I'm </p>
-            <h1> Katherine</h1>
+            <h1> Katherine Li</h1>
             <h2> Computer Science & Immersive Media Design @ University of Maryland </h2>
 
             <div className="buttons">
-                <a href="#experience">Experience</a>
-                <a href="#projects">Projects</a>
-                
+                <a className="button-link" href="#experience">Experience</a>
+                <a className="button-link" href="#projects">Projects</a>
             </div>
             
-            <Contact />
-            <div className="mobile-img">
+            
+            <div className="katherine-img">
               <img src={Katherine}/>
             </div>
         </div>
 
-        <div className="home-section" id="home-img">
-            <img src={Katherine}/>
-  </div>
         
     </div>
   );
