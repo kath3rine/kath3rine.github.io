09ba57016eecee8f1e0a0b9e882d9f42812f5c1d
kath3rine <not.katherine.li@gmail.com>
2023-07-23 01:42:47 -0400

    content + homescreen update

--- src/components/ProjCard.js
+++ src/components/ProjCard.js
@@ -8,6 +8,20 @@ function ProjCard({card}) {
         setShowBack(!showBack); 
     } 
 
+    const techColors = {
+      "Python": 'tcol1',
+      "JavaScript": 'tcol1',
+      "HTML/CSS": 'tcol1',
+      "HTML": 'tcol1',
+      "CSS": 'tcol1',
+      "C#": 'tcol1',
+      "Flask": 'tcol2',
+      "React": 'tcol2',
+      "Unity": 'tcol2',
+      "Spotify API": 'tcol2',
+      "scikit-learn": 'tcol2'
+    };
+
     return (
       <div className="flip-card-outer"
       onClick={handleClick}>
@@ -16,13 +30,22 @@ function ProjCard({card}) {
           <div className="card front">
             <img className="proj-img" src={card.img}/>
             <h2> {card.title} </h2>
-            <p className="proj-tech"> {card.tech} </p>
+            <div className="proj-tech">
+              {card.tech.map((t) => (
+                <label className={`${techColors[t]}`}>{t}</label>
+              ))} 
+            </div>
+            <div className="proj-tech">
+              {card.tech2.map((t) => (
+                <label className={`${techColors[t]}`}>{t}</label>
+              ))} 
+            </div>
             <a href={card.link}> LINK </a>
           </div>
           <div className="card back">
-            <p> {card.desc} </p>
-            <p> {card.desc2} </p>
-            <p> {card.desc3} </p>
+              {card.desc.map((d) => (
+                <p>{d}</p>
+              ))}
           </div>
         </div>
       </div>
