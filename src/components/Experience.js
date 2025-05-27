7c2174489c425b7dd3a89d963e94150d0bc83d95
kath3rine <not.katherine.li@gmail.com>
2025-05-26 23:03:52 -0400

    details

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -24,10 +24,10 @@ function Experience() {
                 role: "Software Engineer Intern",
                 date: "JUN 2024 - AUG 2024",
                 desc: [
-                    "◇ Created a tool for managing and manipulating documents to streamline availability and incident management",
-                    "◇ Implemented HTTP requests for a custom API using C# in the .NET framework for efficient interactions with CosmosDB",
-                    "◇ Developed the user interface with React TypeScript, customizable for any user’s specific needs",
-                    "◇ Incorporated error-handling mechanisms to reduce human error and environmental misconfiguration"
+                    "Created a tool for managing and manipulating documents to streamline availability and incident management",
+                    "Implemented HTTP requests for a custom API using C# in the .NET framework for efficient interactions with CosmosDB",
+                    "Developed the user interface with React TypeScript, customizable for any user’s specific needs",
+                    "Incorporated error-handling mechanisms to reduce human error and environmental misconfiguration"
                 ]
             }
         ],
@@ -37,9 +37,9 @@ function Experience() {
                 role: "Teaching Assistant",
                 date: "JAN 2024 - DEC 2024",
                 desc: [
-                    "◇ CMSC351: Algorithms and CMSC250: Discrete Structures",
-                    "◇ Teach discussion sections and hold office hours to guide students and enhance content comprehension",
-                    "◇ Grade and write assignments for 200-600 students covering logic, proofs, probability, graphs, complexity, etc."
+                    "CMSC351: Algorithms and CMSC250: Discrete Structures",
+                    "Teach discussion sections and hold office hours to guide students and enhance content comprehension",
+                    "Grade and write assignments for 200-600 students covering logic, proofs, probability, graphs, complexity, etc."
                 ]
             }, 
             {
@@ -47,9 +47,9 @@ function Experience() {
                 role: "Research Assistant",
                 date: "AUG 2022- DEC 2022",
                 desc: [
-                    "◇ @ Computational Computational Cognitive Neuroscience & Psychiatry Lab",
-                    "◇ Conducted computational modeling of information processing in learning, decision-making, and episodic memory",
-                    "◇ Processed EEG data from recognition memory tasks and developed machine learning classifiers in Python with scikit-learn and TensorFlow to extract relevant features, achieving an ~80% accuracy rate in predicting subjects’ responses"
+                    "@ Computational Computational Cognitive Neuroscience & Psychiatry Lab",
+                    "Conducted computational modeling of information processing in learning, decision-making, and episodic memory",
+                    "Processed EEG data from recognition memory tasks and developed machine learning classifiers in Python with scikit-learn and TensorFlow to extract relevant features, achieving an ~80% accuracy rate in predicting subjects’ responses"
                 ]
             }
         ],
@@ -59,9 +59,9 @@ function Experience() {
                 role: "Neuromorphic Computing Researcher",
                 date: "MAY 2023 - MAY 2024",
                 desc: [
-                    "◇ Optically perturb and computationally model living neural network dynamics for a hybrid computing I/O platform", 
-                    "◇ Culture cells and use optogenetic inhibition and activation via SLM to form and modulate living neural networks, and monitor neuronal activity with fluorescence microscopy, calcium imaging, and image segmentation",
-                    "◇ Leverage Python, TypeScript, Docker, web sockets, and high-performance computing to create an app enabling custom photostimulation and real-time image and signal analysis on a digital micromirror device"
+                    "Optically perturb and computationally model living neural network dynamics for a hybrid computing I/O platform", 
+                    "Culture cells and use optogenetic inhibition and activation via SLM to form and modulate living neural networks, and monitor neuronal activity with fluorescence microscopy, calcium imaging, and image segmentation",
+                    "Leverage Python, TypeScript, Docker, web sockets, and high-performance computing to create an app enabling custom photostimulation and real-time image and signal analysis on a digital micromirror device"
                 ]
             }
         ],
@@ -71,8 +71,8 @@ function Experience() {
                 role: "Tech Consultant",
                 date: "MAR 2023 - MAY 2024",
                 desc: [
-                    "◇ Applied software development skills to advise tech startups as a member of a competitive business club",
-                    "◇ Collaboratively built a full-stack app for an edutech startup, utilizing React Native for the user interface and allowing users to schedule appointments, search through a database, chat, and video conference"
+                    "Applied software development skills to advise tech startups as a member of a competitive business club",
+                    "Collaboratively built a full-stack app for an edutech startup, utilizing React Native for the user interface and allowing users to schedule appointments, search through a database, chat, and video conference"
                 ]
             }
         ]
@@ -88,7 +88,7 @@ function Experience() {
                             <div id="sidebar-company"
                             key={index}
                             onClick={() => handleChange(index)}>
-                                <a className={`${item==index? 'selected' : 'not-selected'}`}>{group[0].company}</a>
+                                <li className={`${item==index? 'selected' : 'not-selected'}`}><a >{group[0].company}</a></li>
                             </div>
                         ))}
                     </div>
@@ -103,7 +103,7 @@ function Experience() {
 
                                 <div id="job-desc">
                                     {x.desc.map((d) => (
-                                        <p id="job-desc-item">{d}</p>
+                                        <li id="job-desc-item">{d}</li>
                                     ))}
                                 </div>
                             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,10 +11,10 @@ function Projects() {
             title: "Tinderfy",
             tech: ["Flask", "Python", "HTML", "CSS"],
             desc: [
-                "◇ Quasi-satirical fake dating app centered around music taste compatibility assessment",
-                "◇ Developed simulated Tinder profiles showing joint music recommendations, favorite artists/genres, and shared content",
-                "◇ Utilized Pandas and Spotify API for user authorization, data scraping, processing, and analysis",
-                "◇ Used decision trees from scikit-learn to calculate the similarity of users’ favorite songs, enhancing the algorithm’s accuracy"
+                "Quasi-satirical fake dating app centered around music taste compatibility assessment",
+                "Developed simulated Tinder profiles showing joint music recommendations, favorite artists/genres, and shared content",
+                "Utilized Pandas and Spotify API for user authorization, data scraping, processing, and analysis",
+                "Used decision trees from scikit-learn to calculate the similarity of users’ favorite songs, enhancing the algorithm’s accuracy"
             ],
             link: "https://www.github.com/kath3rine/tinderfy"
         },
@@ -23,21 +23,21 @@ function Projects() {
             title: "Impost-AR",
             tech: ["Unity", "C#", "Maya"],
             desc: [
-                "◇ Collaborative senior capstone project",
-                "◇ Multiplayer augmented reality escape room / scavenger hunt based loosely off 'And Then There Were None'",
-                "◇ Players solve virtual and physical puzzles to uncover clues, stay alive, and figure out which player is the imposter",
-                "◇ Implemented networking with Photon Engine for seamless multiplayer interaction",
-                "◇ Designed various engaging blended-reality mini-games and integrated them into the gameplay"
+                "Collaborative senior capstone project",
+                "Multiplayer augmented reality escape room / scavenger hunt based loosely off 'And Then There Were None'",
+                "Players solve virtual and physical puzzles to uncover clues, stay alive, and figure out which player is the imposter",
+                "Implemented networking with Photon Engine for seamless multiplayer interaction",
+                "Designed various engaging blended-reality mini-games and integrated them into the gameplay"
             ],
             link: "https://sites.google.com/terpmail.umd.edu/emma-and-katherine-capstone"
         },
         {
             img: Portfolio,
-            title: "Portfolio Website",
+            title: "Portfolio Site",
             tech: ["React", "HTML", "CSS", "JavaScript"],
             desc: [
-                "◇ This website! Designed and coded an engaging portfolio website from scratch, featuring more projects and info",
-                "◇ Implemented responsiveness for a smoother user experience, and used component-based architecture for scalability"
+                "This website! Designed and coded an engaging portfolio website from scratch, featuring more projects and info",
+                "Implemented responsiveness for a smoother user experience, and used component-based architecture for scalability"
             ],
             link: "https://www.kath3rine.github.io"
         }
@@ -46,6 +46,7 @@ function Projects() {
     <div className="section" id="project-section">
         <div className="section-title" id="projects">
                 <h1 className="section-title"> ~/projects </h1>
+                <p id="proj-intro">print("click on each project card to learn more")</p>
                 <div id="projects-content">
                 {projects.map((card) => (
                     <ProjCard card={card} />
